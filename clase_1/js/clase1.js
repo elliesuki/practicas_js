4559f86a4e7aad8fcd240370d341e02f615ba80b
cadif1 <cadif1@hotmail.com>
2020-01-30 12:21:16 -0400

    Primera claseç

---
+++ clase_1/js/clase1.js
@@ -0,0 +1,9 @@
+/*  if (confirm("me perd0nas?")){
+     alert("DE PINGA")
+ }else{
+     alert("Ah bueno te me cuidas")
+ } */
+console.log("A mi no me compares que yo tengo pa comprar la competencia" + " A todo el movimiento entero con su descendencia");
+var variable1 = "var";
+var variable2 = 20;
+console.log(variable2)
---
+++ clase_1/js/otro_script.js
@@ -0,0 +1,5 @@
+console.log("Script nuevo");
+//Crear nùmero aleatorio entre 0 y 20
+var num1=Math.random()*20;
+console.log("n= "+ num1);
+console.log("Fin del script");
